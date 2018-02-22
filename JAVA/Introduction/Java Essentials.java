1dd0923b15e3822cabc6966386db95780235a843
T.MAHARANI <maharani1118@gmail.com>
2018-02-22 20:17:04 +0530

    Create Java Essentials.java

---
+++ JAVA/Introduction/Java Essentials.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+import java.util.*;
+
+public class CandidateCode {
+
+   public static void main(String args[] ) throws Exception {
+
+
+	//Write code here
+
+
+    String str;
+     BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+     for(int i=0;i<5;i++){
+         str=br.readLine();
+        System.out.println(str);
+      }	  
+ 
+  }
+
+ }
